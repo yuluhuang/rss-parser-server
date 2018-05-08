ae63bd3b9206748bd4d11e4eb5036723e8cb1d2e
yuluhuang <504367857@qq.com>
2018-05-08 09:50:16 +0800

    init

---
+++ core/api/rss.js
@@ -0,0 +1,49 @@
+(function () {
+    "use strict";
+    const debug = require('debug')('app4');
+    const feedparser = require('feedparser-promised');
+    const RssParser = require('rss-parser');
+    let rssParser = new RssParser();
+
+    exports.parser = function (req, res) {
+        let body = Object.assign({}, req.body, req.params, req.query);
+
+        (async () => {
+
+            if (body.xmlurl) {
+                try {
+                    let _feed = await rssParser.parseURL(body.xmlurl);
+                    return res.send({
+                        code: 200,
+                        data: _feed
+                    });
+
+                } catch (e) {
+                    console.error(e.message, '继续使用 feedparser 抓取')
+                    feedparser.parse({uri: body.xmlurl, timeout: 10000}).then(function (items) {
+                        return res.send({
+                            code: 200,
+                            data: items
+                        });
+                    }).catch(function (err) {
+                        return res.send({
+                            code: 500,
+                            info: err.message
+                        });
+                    })
+                }
+
+            } else {
+                return res.send({
+                    code: 500,
+                    info: 'url 不存在'
+                });
+            }
+
+
+        })();
+
+    };
+
+
+})();
---
+++ core/index.js
@@ -0,0 +1,34 @@
+/*global exports,require,process*/
+(function() {
+    "use strict";
+
+    /**
+     * Init
+     * 获取user 绑在req上
+     * @param req
+     * @param res
+     * @param next
+     */
+    exports.init = async function(req, res, next) {
+
+
+    };
+
+    /**
+     * check auth
+     * @param req
+     * @param res
+     * @param next
+     */
+    exports.check_auth = function(req, res, next) {
+
+    };
+    /**
+     * check auth
+     * @param req
+     * @param res
+     * @param next
+     */
+    exports.check_admin_auth = function(req, res, next) {
+    };
+})();
